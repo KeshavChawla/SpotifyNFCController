74d6f793bb987db290789ccfed5cdc9005102b5c
Keshav Chawla <keshavchawla2014@gmail.com>
2022-07-29 23:30:50 -0400

    fix: add spotify auth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
+import { SpotifyApiContext } from 'react-spotify-api'
+import { SpotifyAuth, Scopes } from 'react-spotify-auth'
+import 'react-spotify-auth/dist/index.css' // if using the included styles
+import Cookies from 'js-cookie'
+
+
+
 
 function App() {
+  const [token, setToken] = React.useState(Cookies.get("spotifyAuthToken"))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {token ? (
+        <SpotifyApiContext.Provider value={token}>
+          {/* Your Spotify Code here */}
+          <p>You are authorized with token: {token}</p>
+        </SpotifyApiContext.Provider>
+      ) : (
+        // Display the login page
+        <SpotifyAuth
+          redirectUri='http://localhost:3000/callback'
+          clientID='8fe16d0e58934415a6fa4fbc934c4944'
+          scopes={[Scopes.userReadPrivate, 'user-read-email']} // either style will work
+          onAccessToken={(token) => setToken(token)}
+        />
+      )}
     </div>
+
   );
 }
 
