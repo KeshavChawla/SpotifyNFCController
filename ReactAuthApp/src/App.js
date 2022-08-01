7575e5598ba344fda1e57de660bbd1b4675b82fd
Keshav Chawla <keshavchawla2014@gmail.com>
2022-08-01 04:35:04 -0400

    extract node server url

--- ReactAuthApp/src/App.js
+++ ReactAuthApp/src/App.js
@@ -5,11 +5,12 @@ import { SpotifyAuth, Scopes } from 'react-spotify-auth'
 import 'react-spotify-auth/dist/index.css' // if using the included styles
 import Cookies from 'js-cookie'
 
+const nodeServerUrl = 'http://192.168.0.38:3031'
 
 async function updateFunction(token) {
   const response =
     await fetch(
-      `http://192.168.0.20:3031/tokenPost?token=${token}`,
+      `${nodeServerUrl}/tokenPost?token=${token}`,
       {
         method: 'POST',
       }
